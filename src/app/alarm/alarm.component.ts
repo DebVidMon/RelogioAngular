63f37f373020e6d2e6d7991c177ab4c7413cbade
Debora <debvm@hotmail.com>
2021-06-16 18:06:01 -0300

    feat(alarm): Botão On/Off

--- src/app/alarm/alarm.component.ts
+++ src/app/alarm/alarm.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-alarm',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alarm.component.css']
 })
 export class AlarmComponent implements OnInit {
-
   constructor() { }
-
+  checked: boolean;
+  status: string;
+  ONF: string;
   ngOnInit(): void {
+    this.checked = true;
+    this.liga();
+  }
+  // on toggle method
+  // to check status of checkbox
+  liga() {
+    this.checked = !this.checked;
+    // check if checkbox is checked
+    if (this.checked === true) {
+      // if checked
+      this.status = 'on';
+      this.ONF = 'On';
+    } else {
+      // if unchecked
+      this.status = 'off';
+      this.ONF = 'Off';
+    }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { MenuComponent } from './menu/menu.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    TooltipModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
