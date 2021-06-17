90a85d340844cb640b54fc879a0561a9b394a19a
Debora <debvm@hotmail.com>
2021-06-16 23:26:16 -0300

    feat(alarm):Hora do Alarme

--- src/app/alarm/alarm.component.ts
+++ src/app/alarm/alarm.component.ts
@@ -1,6 +1,5 @@
-
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';;
 
 
 @Component({
@@ -10,12 +9,30 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 })
 export class AlarmComponent implements OnInit {
   constructor() { }
+
   checked: boolean;
   status: string;
   ONF: string;
+
+  horario = new Date();
+  intervalId:any;
+  subscription: Subscription;
+
+  hora: number;
+  minuto: number;
+
+  isShown: boolean = false; // hidden by default
+  isHidden: boolean = true; // shown by default
+
   ngOnInit(): void {
     this.checked = true;
     this.liga();
+
+    // Using Basic Interval
+    this.intervalId = setInterval(() => {
+      this.horario = new Date();
+    }, 1000);
+
   }
   // on toggle method
   // to check status of checkbox
@@ -30,6 +47,30 @@ export class AlarmComponent implements OnInit {
       // if unchecked
       this.status = 'off';
       this.ONF = 'Off';
+      this.hora = 0;
+      this.minuto = 0;
+    }
+  }
+
+  OK(){
+    this.isShown = !this.isShown;
+    this.isHidden = !this.isHidden;
+  }
+
+  tocaAlarme(){
+    let hora1 = this.horario.getUTCHours();
+    let minuto1 = this.horario.getUTCMinutes();
+    if(hora1 === this.hora && minuto1 === this.minuto){
+      //toca alarme
+      alert("toca alarme")
+    }
+  }
+
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
   }
 
